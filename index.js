7952daa0e3f3263397601b09f20134b3d8bf2091
Sukina AbuHammad <sukina_89@yahoo.com>
2021-07-05 00:30:17 +0300

    edit

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 const port = process.env.PORT;
 const server = require('./src/server');
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser : true,
   useUnifiedTopology : true,
 }).then (()=>{
